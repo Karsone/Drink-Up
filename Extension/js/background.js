dd8031664c6106546f28a340b190ebe842e035a6
Karson Enns <hello@karsonenns.com>
2014-09-30 19:29:37 -0700

    Commit of Server code

---
+++ Extension/js/background.js
@@ -0,0 +1,175 @@
+drinkUp = {
+  init: function(){
+    drinkUp.socketConnection();
+    drinkUp.newOffer();
+    drinkUp.offerDrink();
+    drinkUp.iWantDrink();
+    drinkUp.serverCountdown();
+    drinkUp.changeDesire();
+    drinkUp.changeName();
+    drinkUp.changeCellNumber();
+
+    //LocalStorage Defaults
+
+    chrome.storage.sync.get("name", function(storage){
+      if(!storage.name){
+        chrome.storage.sync.set({"name":"None"});
+      }
+    });
+
+    chrome.storage.sync.get("desire", function(storage){
+      if(!storage.desire){
+        chrome.storage.sync.set({"desire":"Want to"});
+      }
+    });
+  },
+  iWantDrink: function(){
+    chrome.extension.onMessage.addListener( function(data){
+      if(data.action == "iWantDrink"){
+        socket.emit('iWantDrink', data);
+      }
+    });
+  },
+  serverCountdown: function(){
+    var drinkers = [];
+    socket.on('newDrinker', function (data) {
+      drinkers.push(data);
+    });
+
+    chrome.extension.onMessage.addListener( function(data){
+      if(data.action == "serverCountdown"){
+        drinkers = [];
+        secondsLeft = 61;
+        var timeout = function(){
+          setTimeout( function(){
+            if(secondsLeft > 1){
+              secondsLeft = secondsLeft-1;
+              timeout();
+            }else{
+              drinkerString = "";
+              drinkers.forEach(function(element, index, array){
+                drinkerString += "\n - "+ element.user;
+              });
+
+              chrome.storage.sync.get("cellNumber", function(storage){
+                console.log(storage);
+                data.cellNumber = storage.cellNumber;
+                data.textMessage = "You and "+drinkers[drinkers.length-1].user+" are making "+drinkers[drinkers.length-1].drinkName+"! For "+drinkerString+"";
+                socket.emit('textMessage', data);
+
+                var notification = webkitNotifications.createNotification("", "You and "+drinkers[drinkers.length-1].user+" are making "+drinkers[drinkers.length-1].drinkName+"!", "For "+drinkerString+"");
+                notification.show();
+                drinkers = [];
+              });
+
+
+            }
+          },1000);
+        }
+        timeout();
+      }
+    });
+  },
+  socketConnection: function(){
+    //Initial Socket Connection
+    socket = io.connect('http://knode.cloudapp.net:3001/');
+  },
+  offerDrink: function(drink){
+    //User Offers a drink, message goes to server.
+    chrome.extension.onMessage.addListener( function(data){
+      if(data.action == "offerDrink"){
+        socket.emit('offerDrink', data);
+      }
+    });
+  },
+  newOffer: function(){
+    //Someone else has offered a drink!
+    socket.on('newOffer', function (data) {
+      data.action = "newOffer";
+      chrome.storage.sync.get("name", function(storage){
+        currentUser = storage.name;
+      });
+
+      chrome.storage.sync.get("desire", function(storage){
+        if(storage.desire == "Want to"){
+
+          if(currentUser == data.user){
+
+            var notification = webkitNotifications.createNotification("","Offer Sent!" ,"You sent an offer for" + data.drinkName);
+            notification.show();
+
+            setTimeout(function(){
+              notification.cancel();
+            },2000)
+
+            chrome.extension.sendMessage({
+              "action":"serverCountdown"
+            });         
+
+            chrome.extension.sendMessage({
+              "action":"iWantDrink",
+              drinkID: data.drinkID,
+              drinkName: data.drinkName,
+              user: data.user,
+              currentUser:currentUser
+            });
+
+          }else{
+
+            var notification = webkitNotifications.createNotification("","New Drink Offer!",data.user+" is offering " + data.drinkName +"\n \n[Click to Accept]");
+            notification.show();
+
+
+            notification.onclick = function() {
+              // // Handle action from notification being clicked.
+              chrome.storage.sync.get("name", function(storage){
+                chrome.extension.sendMessage({
+                  "action":"iWantDrink",
+                  drinkID: data.drinkID,
+                  drinkName: data.drinkName,
+                  user: storage.name
+                });
+              });
+            }
+
+            setTimeout(function(){
+              notification.cancel();
+            },60000)
+
+          }
+
+        }
+      });
+    });
+  },
+  changeName: function(){
+    chrome.extension.onMessage.addListener( function(data){
+      if(data.action == "changeName"){
+        if(data.name != "none"){
+          chrome.storage.sync.set({"name":data.name});
+        }else{
+          alert("Gotta have a name!");
+        }
+      }
+    });
+  },
+  changeCellNumber: function(){
+    chrome.extension.onMessage.addListener( function(data){
+      if(data.action == "changeCellNumber"){
+        chrome.storage.sync.set({"cellNumber":data.cellNumber});
+      }
+    });
+  },
+  changeDesire: function(){
+    chrome.extension.onMessage.addListener( function(data){
+      if(data.action == "changeDesire"){
+        chrome.storage.sync.set({"desire":data.desire});
+      }
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  drinkUp.init();
+});
+
---
+++ Server/app.js
@@ -0,0 +1,51 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(3001);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+
+io.sockets.on('connection', function (socket) {
+  console.log("Connected");
+  //When someone offers a drink.
+  //Data - drink,user.
+  socket.on('offerDrink', function (data) {
+    io.sockets.emit('newOffer', data);
+    // socket.broadcast.emit('newOffer', data); //To all clients but the current one
+  });
+  socket.on('iWantDrink', function (data) {
+    io.sockets.emit('newDrinker', data);
+    // socket.broadcast.emit('newOffer', data); //To all clients but the current one
+  });
+  socket.on('textMessage', function (data) {
+    // io.sockets.emit('newDrinker', data);
+    // Your accountSid and authToken from twilio.com/user/account
+    var accountSid = 'AC5060a2b2afd610be8c9c608e567ff061';
+    var authToken = "48e016c34a034a14e23aa83246df76f0";
+    var client = require('twilio')(accountSid, authToken);
+
+    if(data.cellNumber){
+      client.messages.create({
+          body: data.textMessage,
+          to: "+1"+data.cellNumber,
+          from: "+17787650037"
+      });
+    }
+  });
+});
+
+
+
